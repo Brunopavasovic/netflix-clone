60ae9dea2bd0eb2ff377f9f5faec9664badbc510
Bruno Pavasović <brunopavasovic@gmail.com>
2022-06-29 23:50:34 +0200

    LoginPage brightness fixed and added form

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,21 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+
+export const LoginForm = () => {
+  return (
+    <Box
+      zIndex="1"
+      position="absolute"
+      w="20vw"
+      h="70vh"
+      backgroundColor="blackAlpha.800"
+      m="0 auto"
+      display="inline"
+      top="0"
+      right="0"
+      left="0"
+      bottom="0"
+      margin="auto"
+    ></Box>
+  );
+};
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -3,20 +3,21 @@ import React from 'react';
 import img1 from '../images/ntflix-bg.png';
 import logo from '../images/ntflix-logo.png';
 import { Img } from '@chakra-ui/react';
+import { LoginForm } from './LoginForm';
 
 export const LoginPage = () => {
   return (
-    <Box
-      w="100vw"
-      h="100vh"
-      backgroundImage={img1}
-      bgRepeat="no-repeat"
-      bgSize="cover"
-      filter="auto"
-      brightness="60%"
-      zIndex="1"
-    >
-      <Img w="300px" zIndex="-1" h="150px" src={logo}></Img>
+    <Box w="100vw" h="100vh">
+      <LoginForm />
+      <Img w="100vw" h="100vh" src={img1} filter="auto" brightness="50%"></Img>
+      <Img
+        src={logo}
+        w="200px"
+        h="100px"
+        position="absolute"
+        mt="-100vh"
+        ml="50px"
+      ></Img>
     </Box>
   );
 };
